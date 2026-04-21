fa31fd62159247354ad53fe3ebd35e8bbe3ede7e
Kain027 <tranhoangbaokhanh120@gmail.com>
2026-04-21 23:25:29 +0700

    feat: Responsive interface, login/registration complete.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,16 @@ import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 const App = () => {
+
+const [showLogin,setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from "../../assets/assets";
 
 
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
     const [menu, setMenu] = useState("home");
     return (
         <div className="navbar">
@@ -21,7 +21,7 @@ const Navbar = () => {
                     <img src={assets.basket_icon} alt="" />
                     <div className="dot"></div>
                 </div>
-                <button>Sign in</button>
+                <button onClick={()=>setShowLogin(true)}>Sign in</button>
             </div>
         </div>
     )
